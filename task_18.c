12b04e52e4d80d56038360c2471f2093c03c24a6
snippetsx <snippetsx@lcsa.ru>
2023-03-18 13:38:45 +0300

    Addded new tasks

---
+++ task_18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n, m;
+    scanf("%d" "%d", &n, &m);
+    if(n > m){
+    	printf("%d", n);
+    }
+    else if(m > n){
+    	printf("%d", m);
+    }
+    else{
+	printf("%d", m);
+    }
+    return 0;
+}
