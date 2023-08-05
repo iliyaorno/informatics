805151c91538a877448b270ed99c115674ca3f20
snippetsx <snippetsx@lcsa.ru>
2023-08-05 11:12:29 +0300

    add 8.c

---
+++ Поиск и сортировка/8.c
@@ -0,0 +1,24 @@
+// https://informatics.msk.ru/mod/statements/view.php?id=270&chapterid=1440#1
+#include <stdio.h>
+
+int main()
+{
+    int m1;
+    int m2;
+    int n;
+    int k;
+    scanf ("%d" "%d", &n, &k);
+    m1 = k;
+    m2 = 0;
+    for (int i = 1; i < n; i++){
+        scanf ("%d", &k);
+        if (k > m1) { 
+            m2 = m1;
+            m1 = k;
+        }
+        if (k > m2 && k < m1){ 
+            m2 = k;
+        }
+    }
+    printf ("%d", m2);
+}
