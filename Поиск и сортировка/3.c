54a3458b861dd5d85f22f0c9db2f2cfcefc3f212
snippetsx <snippetsx@lcsa.ru>
2023-08-05 10:29:50 +0300

    add 3.c

---
+++ Поиск и сортировка/3.c
@@ -0,0 +1,22 @@
+// https://informatics.msk.ru/mod/statements/view.php?id=270&chapterid=225#1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    int x;
+    int k;
+    scanf ("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++){
+        scanf ("%d", &a[i]);
+    }
+    scanf ("%d", &x);
+    for (int i = 0; i < n; i++){
+        if (abs (a[i] - x) < abs (k - x) || abs (a[i] - x) == abs (k - x) && a[i] < k){
+            k = a[i];
+        }
+    } 
+    printf("%d", k);
+    return 0;
+}
